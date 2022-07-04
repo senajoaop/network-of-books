fa4b08df2ce724e51452218c0b64d72bcc33f323
senajoaop <sena.joaopedro42@gmail.com>
2022-07-04 17:21:44 -0300

    Creation of the main class and imports

---
+++ BookNetwork.py
@@ -0,0 +1,23 @@
+import pdfplumber
+import pandas as pd
+import numpy as np
+import spacy
+from spacy import displacy
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+import re
+from pyvis.network import Network
+from sympy import N, degree
+import community as community_louvain
+
+
+
+class BookNetwork:
+    def __init__(self):
+        pass
+
+
+
+if __name__=="__main__":
+    BookNetwork()
