3c5e8ab5d3e38f08354a1c5fec8efe5693b5e405
senajoaop <sena.joaopedro42@gmail.com>
2022-07-04 17:56:12 -0300

    Transform pdf to text method

--- BookNetwork.py
+++ BookNetwork.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import pdfplumber
 import pandas as pd
 import numpy as np
@@ -17,6 +18,17 @@ class BookNetwork:
     def __init__(self):
         pass
 
+    def get_text_from_pdf(filePath, fileName, pages=None):
+        bookText = ""
+
+        with pdfplumber.open(filePath) as pdf:
+            for page in pdf.pages:
+                bookText += page.extract_text() + "\n"
+
+            bookText = bookText.replace("\t", " ")
+
+        with open(fileName, 'w') as file:
+            file.write(bookText)
 
 
 if __name__=="__main__":
